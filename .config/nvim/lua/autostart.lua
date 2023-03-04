270f80c23a5185a34d33458af3ab4e999f7c19fd
Konstantin Zhigaylo <kostyantop8@gmail.com>
2023-03-04 09:54:00 +0300

    Update Neovim config

--- .config/nvim/lua/autostart.lua
+++ .config/nvim/lua/autostart.lua
@@ -3,7 +3,7 @@
 
 -- Startup dashboard-nvim
 if next(vim.fn.argv()) == nil then
-    require'alpha'.setup(require('alpha.themes.zero').config)
+    require'alpha'.setup(require('alpha.themes.theta').config)
 end
 
 vim.cmd("hi NonText guifg=bg")
